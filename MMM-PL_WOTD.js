f662596712ef2173de40bc133f6fe9dbf5b187de
Matt <mbatko0914@gmail.com>
2020-07-12 11:47:10 -0400

    rewrite of init file

--- MMM-PL_WOTD.js
+++ MMM-PL_WOTD.js
@@ -1,116 +1,199 @@
-Module.register('MMM-PL_WOTD', {
+/* global Module */
 
-  defaults: {
-    loaded: false,
-    horizontal: false,
-    wotd: {}
+/* Magic Mirror
+ * Module: MMM-PL_WOTD
+ *
+ * By mjb527
+ */
 
-  }
+Module.register("MM-PL_WOTD", {
+	defaults: {
+		updateInterval: 24 * 60 * 60 * 1000, // update once per day
+		retryDelay: 5000,
+		wotd: null
+	},
 
-  start: function() {
-    Log.log('Starting module ' + this.name);
+	requiresVersion: "2.1.0", // Required version of MagicMirror
 
-    // make initial request
-    this.wotd = this.getWord();
+	start: function() {
+		Log.log("Starting module " + this.name);
 
-  },
+		var self = this;
 
-  // return word of the day data
-  getWord: function() {
+		//Flag for check if module is loaded
+		this.loaded = false;
 
-    // requests/webscraping
-    const rp = require('request-promise');
-    const cheerio = require('cheerio');
+		// Schedule update timer.
+		this.getData();
+		setInterval(function() {
+			self.updateDom();
+		}, this.config.updateInterval);
+	},
 
-    // config the packages
-    const options = {
-      uri: `https://www.polishpod101.com/polish-phrases/`,
-      transform: function (body) {
-        return cheerio.load(body);
-      }
-    };
+	/*
+	 * getData
+	 * function example return data and show it in the module wrapper
+	 * get a URL request
+	 *
+	 */
+	getData: function() {
+		var self = this;
 
-    const wotd_data = {};
+		const rp = require("request-promise");
+		const cheerio = require("cheerio");
 
-    rp(options)
-    .then(function (data) {
-      const $ = cheerio.load(data._root.children[1].children);
+		// config the packages
+		const options = {
+			uri: "https://www.polishpod101.com/polish-phrases/",
+			transform: function (body) {
+				return cheerio.load(body);
+			}
+		};
 
-      // rows of Polish and English translation
-      const pRows = $('.r101-wotd-widget__word');
-      const eRows = $('.r101-wotd-widget__english');
+		const wotdData = {};
 
-      const word = formatText(pRows[0].children[0].data);
-      const translation = formatText(eRows[0].children[0].data);
-      const partOfSpeech = formatText($('.r101-wotd-widget__class').text().trim());
+		rp(options)
+			.then(function (data) {
+				const $ = cheerio.load(data._root.children[1].children);
 
-      const examples = [];
+				// rows of Polish and English translation
+				const pRows = $(".r101-wotd-widget__word");
+				const eRows = $(".r101-wotd-widget__english");
 
-      console.log(`${word} - ${translation} - ${partOfSpeech}`);
+				const word = formatText(pRows[0].children[0].data);
+				const translation = formatText(eRows[0].children[0].data);
+				const partOfSpeech = formatText($(".r101-wotd-widget__class").text().trim());
 
-      for(let i = 1; i < pRows.length; i++) {
-        // get the text, capitalize first letter
-        let polish = formatText(pRows[i].children[0].data);
-        let english = formatText(eRows[i].children[0].data)
+				const examples = [];
 
-        examples.push({"polish" : polish,
-        "english" : english});
-        console.log(examples[i-1].polish + ' - ' + examples[i-1].english);
-      }
+				console.log(`${word} - ${translation} - ${partOfSpeech}`);
 
-      // in 2 parts to keep incomplete data from being saved to obj
-      wotd_data.word = word;
-      wotd_data.translation = translation;
-      wotd_data.partOfSpeech = partOfSpeech;
-      wotd_data.examples = examples;
+				for(let i = 1; i < pRows.length; i++) {
+				// get the text, capitalize first letter
+					let polish = formatText(pRows[i].children[0].data);
+					let english = formatText(eRows[i].children[0].data)
 
-      // save data
-      return wotd_data;
+					examples.push({"polish" : polish,
+						"english" : english});
+					console.log(examples[i-1].polish + " - " + examples[i-1].english);
+				}
 
-    })
-    .catch(function (err) {
-      Log.log(err);
-      // TODO write error to page instead of the word of the day stuff
-      wotd = {'word': 'Error getting word of the day'};
-      // REQUEST FAILED: ERROR OF SOME KIND
-    });
+				// in 2 parts to keep incomplete data from being saved to obj
+				wotd_data.word = word;
+				wotd_data.translation = translation;
+				wotd_data.partOfSpeech = partOfSpeech;
+				wotd_data.examples = examples;
 
-    // we have data
-    this.loaded = true;
+				// save data
+				this.wotdData = wotdData;
 
-  },
+			})
+			.catch(function (err) {
+				Log.log(err);
+				// TODO write error to page instead of the word of the day stuff
+				wotd = {"word": "Error getting word of the day"};
+			});
+	},
 
-  getDOM: function() {
-    let wrapper = null;
 
-    // check if we have data
-    if(this.loaded) {
+	/* scheduleUpdate()
+	 * Schedule next update.
+	 *
+	 * argument delay number - Milliseconds before next update.
+	 *  If empty, this.config.updateInterval is used.
+	 */
+	scheduleUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+		nextLoad = nextLoad ;
+		var self = this;
+		setTimeout(function() {
+			self.getData();
+		}, nextLoad);
+	},
 
-      wrapper = document.createElement('div');
+	getDom: function() {
+		var self = this;
 
-      const title = document.createElement('h4');
-      const translation = document.createElement('h6');
-      const partOfSpeech = document.createElement('h6');
-      const examples = document.createElement('ul');
+		// create element wrapper for show into the module
+		var wrapper = document.createElement("div");
+		// If this.dataRequest is not empty
+		if (this.wotd) {
+			wrapper = document.createElement("div");
 
-      wrapper.append(title);
-      wrapper.append(translation);
-      wrapper.append(partOfSpeech);
-      wrapper.append(examples);
+			const title = document.createElement("h4");
+			const translation = document.createElement("h6");
+			const partOfSpeech = document.createElement("h6");
+			const examples = document.createElement("ul");
 
-      title.innerHTML = this.wotd.word;
-      translation.innerHTML = this.wotd.translation;
-      partOfSpeech.innerHTML = this.wotd.partOfSpeech;
+			wrapper.append(title);
+			wrapper.append(translation);
+			wrapper.append(partOfSpeech);
+			wrapper.append(examples);
 
-      this.wotd.examples.forEach(example => {
-        const li = createElement('li');
-        li.innerHTML = example;
-        examples.appendChild(li);
-      });
+			title.innerHTML = this.wotd.word;
+			translation.innerHTML = this.wotd.translation;
+			partOfSpeech.innerHTML = this.wotd.partOfSpeech;
 
-    }
+			this.wotd.examples.forEach(example => {
+				const li = createElement("li");
+				li.innerHTML = example;
+				examples.appendChild(li);
+			});
 
-    return wrapper;
-  }
+		}
 
+		return wrapper;
+
+
+		// Data from helper
+		// if (this.dataNotification) {
+		// 	var wrapperDataNotification = document.createElement("div");
+		// 	// translations  + datanotification
+		// 	wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
+		//
+		// 	wrapper.appendChild(wrapperDataNotification);
+		// }
+	},
+
+	// getScripts: function() {
+	// 	return [];
+	// },
+
+	getStyles: function () {
+		return [
+			"MMM-PL_WOTD.css",
+		];
+	},
+
+	// Load translations files
+	// getTranslations: function() {
+	// 	//FIXME: This can be load a one file javascript definition
+	// 	return {
+	// 		en: "translations/en.json",
+	// 		es: "translations/es.json"
+	// 	};
+	// },
+
+	// processData: function(data) {
+	// 	var self = this;
+	// 	this.dataRequest = data;
+	// 	if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
+	// 	this.loaded = true;
+	//
+	// 	// the data if load
+	// 	// send notification to helper
+	// 	this.sendSocketNotification("{{MODULE_NAME}}-NOTIFICATION_TEST", data);
+	// },
+
+	// socketNotificationReceived from helper
+	// socketNotificationReceived: function (notification, payload) {
+	// 	if(notification === "{{MODULE_NAME}}-NOTIFICATION_TEST") {
+	// 		// set dataNotification
+	// 		this.dataNotification = payload;
+	// 		this.updateDom();
+	// 	}
+	// },
 });
